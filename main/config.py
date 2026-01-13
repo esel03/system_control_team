9494e50b87fefa2a021e710ca98e1509c258496a
Ardatik <belogurovigor9@gmail.com>
2026-01-13 18:09:41 +0300

    add table users and delete row in config

--- main/config.py
+++ main/config.py
@@ -9,7 +9,6 @@ class Settings(BaseSettings):
     DB_PORT: int
     DB_NAME: str
 
-    # DATABASE_SQLITE = 'sqlite+aiosqlite:///data/db.sqlite3'
     model_config = SettingsConfigDict(
         env_file=os.path.join(os.path.dirname(os.path.abspath(__file__)), ".env")
     )
---
+++ main/db/models/users.py
@@ -0,0 +1,17 @@
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import Uuid, String, Boolean
+import uuid
+from db.connect import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+    
+    id: Mapped[uuid.UUID] = mapped_column(Uuid, primary_key=True, default=uuid.uuid4)
+    email: Mapped[str] = mapped_column(String(100), nullable=False, unique=True)
+    first_name: Mapped[str] = mapped_column(String(100), nullable=False)
+    last_name: Mapped[str] = mapped_column(String(100), nullable=False)
+    patronymic_name: Mapped[str] = mapped_column(String(100))
+    password: Mapped[str] = mapped_column(String(100), nullable=False)
+    is_active: Mapped[bool] = mapped_column(Boolean, default=True)
+    is_deleted: Mapped[bool] = mapped_column(Boolean, default=False)
