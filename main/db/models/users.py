dab93a3dae22d42119dd3489bb7d5e68055daa2d
Ardatik <belogurovigor9@gmail.com>
2026-01-13 19:57:16 +0300

    comment users && detete fied is_active

--- main/db/models/users.py
+++ main/db/models/users.py
@@ -7,11 +7,24 @@ from db.connect import Base
 class User(Base):
     __tablename__ = "users"
 
-    user_id: Mapped[uuid.UUID] = mapped_column(Uuid, primary_key=True, default=uuid.uuid4)
-    email: Mapped[str] = mapped_column(String(100), nullable=False, unique=True)
-    first_name: Mapped[str] = mapped_column(String(100), nullable=False)
-    last_name: Mapped[str] = mapped_column(String(100), nullable=False)
-    patronymic_name: Mapped[str] = mapped_column(String(100))
-    password: Mapped[str] = mapped_column(String(100), nullable=False)
-    is_active: Mapped[bool] = mapped_column(Boolean, default=True)
-    is_deleted: Mapped[bool] = mapped_column(Boolean, default=False)
+    user_id: Mapped[uuid.UUID] = mapped_column(
+        Uuid, primary_key=True, default=uuid.uuid4, comment="id пользователя"
+    )
+    email: Mapped[str] = mapped_column(
+        String(100), nullable=False, unique=True, comment="email пользователя"
+    )
+    first_name: Mapped[str] = mapped_column(
+        String(100), nullable=False, comment="имя пользователя"
+    )
+    last_name: Mapped[str] = mapped_column(
+        String(100), nullable=False, comment="фамилия пользователя"
+    )
+    patronymic_name: Mapped[str] = mapped_column(
+        String(100), comment="отчество пользователя"
+    )
+    password: Mapped[str] = mapped_column(
+        String(100), nullable=False, comment="пароль пользователя"
+    )
+    is_deleted: Mapped[bool] = mapped_column(
+        Boolean, default=False, comment="флаг удаленности пользователя"
+    )
