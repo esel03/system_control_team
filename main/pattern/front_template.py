69cc7dac738e63b098e8a7cbc9fdcc975bff0a54
egarmin_is <egarmin_is@magnit.ru>
2026-01-13 17:37:33 +0300

    connect db

--- main/pattern/front_template.py
+++ main/pattern/front_template.py
@@ -1,10 +1,9 @@
 from fastapi import APIRouter
 from fastapi.responses import FileResponse
-from pathlib import Path
 
 router = APIRouter(prefix="")
 
+
 @router.get("/")
 def root():
-    file_path = Path(__file__).parent / "templates" / "index.html"
     return FileResponse("main/templates/index.html")
