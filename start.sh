fbed60a2304caf02cff89e557f0c62fd2970f06d
egarmin_is <egarmin_is@magnit.ru>
2026-01-26 21:39:17 +0300

    fix start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
+
+# Применяем миграции к базе данных
+echo "Запуск миграций Alembic..."
 alembic upgrade head
-uvicorn main.main:app --host 0.0.0.0 --port 8000
+
+# Проверяем, успешно ли прошли миграции
+if [ $? -ne 0 ]; then
+    echo "Ошибка при выполнении миграций"
+    exit 1
+fi
+
+echo "Миграции успешно применены"
+
+# Запуск FastAPI-приложения через uvicorn
+echo "Запуск приложения..."
+uvicorn main.main:app --host 0.0.0.0 --port 8000 --reload
